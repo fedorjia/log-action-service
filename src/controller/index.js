91a029146c5c23cb76955a5887f5fce408454678
fedor <332762488@qq.com>
2018-12-25 09:31:13 +0800

    fix

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router({mergeParams: true})
-const { promisify } = require('util')
 const { body, validationResult } = require('express-validator/check')
 
 const service = require('../service/logger')
